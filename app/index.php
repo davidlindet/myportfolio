01b6db637c493bb98fb316756a59cb4d6dd7312c
David Lindet <david.lindet@gmail.com>
2017-09-26 17:52:34 +0200

    Add viewport + update git ignore

--- app/index.php
+++ app/index.php
@@ -18,6 +18,27 @@
     <meta property="og:image" content="http://david-lindet.fr/img/logo.jpg">
     <meta property="og:description" content="Welcome on my portfolio. Here you can discover projects I worked on. Contact me if you have questions.">
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php /*
+         <link rel="apple-touch-icon" sizes="57x57" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/apple-touch-icon-152x152.png">
+        <link rel="icon" type="image/png" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/manifest.json">
+        <link rel="shortcut icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/favicon.ico">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/mstile-144x144.png">
+        <meta name="msapplication-config" content="<?php echo esc_url(get_template_directory_uri()); ?>/tpl/img/icons/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+ */ ?>
     <link rel="stylesheet" href="./public/css/vendor.min.css" />
     <link rel="stylesheet" href="./public/css/app.min.css" />
     <link rel="icon" type="image/png" href="./public/img/favicon.png" />
